551860819fdb6e518c90fd12bf9cc520adcf1739
Andy <antr900@gmail.com>
2020-06-08 22:43:47 -0700

    Finish uploading music

--- antd-demo-ts/src/components/Track/CreateTrack.tsx
+++ antd-demo-ts/src/components/Track/CreateTrack.tsx
@@ -29,58 +29,97 @@ const CreateTrack: React.FC<any> = ({classes}) => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [file,setFile] = useState("")
+    const [progress, setProgress] = useState(0);
     const [submitting, setSubmitting] = useState(false);
     const [modalState, setModal] = useState(false);
+    const [url, setUrl] = useState(null);
     const [createTrack, {loading, error}] = useMutation(CREATE_TRACK_MUTATION)
 
+    const uploadAudio = async (options) => {
+        setSubmitting(true);
+      const { onSuccess, onError, file, onProgress } = options;
+      const fmData = new FormData();
+      const config = {
+        headers: { "content-type": "multipart/form-data" },
+        onUploadProgress: (event) => {
+          const percent = Math.floor((event.loaded / event.total) * 100);
+          setProgress(percent);
+          if (percent === 100) {
+            setTimeout(() => setProgress(0), 1000);
+          }
+          onProgress({ percent: (event.loaded / event.total) * 100 });
+        },
+      };
 
+      debugger
+      fmData.append("file", file);
+      fmData.append("resource_type", "raw");
+      fmData.append("upload_preset", "music-app");
+      fmData.append("cloud_name", "andytran");
+      try {
+        const res = await axios.post(
+          "https://api.cloudinary.com/v1_1/andytran/raw/upload",
+          fmData,
+          config
+        );
 
-    const props = {
-        // action: '//jsonplaceholder.typicode.com/posts/',
-        accept:"audio/mp3, audio/wav",
-        multiple: false,
-
+        onSuccess("Ok");
+        console.log("server res: ", res);
+        setUrl(res.data.url);
+        setSubmitting(false);
+      } catch (err) {
+         console.error("Error uploading file", err);
+         setSubmitting(false);
+      }
     };
 
-    const handleAudioChange = event => {
+    const props = {
+      // action: '//jsonplaceholder.typicode.com/posts/',
+      accept: "audio/mp3, audio/wav",
+      
+    };
+
+    const handleAudioChange = info => {
         debugger;
-        const selectedFile = event.fileList[0];
+        const selectedFile = info.fileList[0]
 
-        console.log(selectedFile);
-
-    
         setFile(selectedFile);
 
     };
 
 
-    const handleAudioUpload = async () => {
-        try {
-            const data = new FormData()
-            data.append('file', file)
-            data.append('resource_type', 'raw')
-            data.append('upload_present', 'music-app')
-            data.append('cloud_name', 'andytran')
-            debugger
-            const res = await axios.post('https://api.cloudinary.com/v1_1/andytran/raw/upload', data)
-            return res.data.url
+    // const handleAudioUpload = async () => {
+    //     try {
+    //         const data = new FormData()
+    //         data.append("file", file)
+    //         data.append("resource_type","raw")
+    //         data.append('upload_preset',"music-app")
+    //         data.append("cloud_name","andytran")
+    //         const res = await axios.post(
+    //           "https://api.cloudinary.com/v1_1/andytran/raw/upload",
+    //           data
+    //         );
+    //         return res.data.url
     
-        } catch(err) {
-            console.error('Error uploading file', err)
-            setSubmitting(false);
-        }
-    }
+    //     } catch(err) {
+    //         console.error('Error uploading file', err)
+    //         setSubmitting(false);
+    //     }
+    // }
 
-    const handleSubmit = async (event, createTrack) => {
+    const handleSubmit = (event, createTrack) => {
         event.preventDefault()
-        setSubmitting(true);
-        const uploadedUrl = await handleAudioUpload()
-
+    
         debugger
         createTrack({
-            variables:{ title, description, url: uploadedUrl}
-        }).then(()=>{
-            setSubmitting(false)
+            variables:{ title, description, url: url}
+        }).then((data)=>{
+            console.log({ data });
+            setSubmitting(false);
+            setModal(false);
+            setTitle("");
+            setDescription("");
+            setFile("");
         })
 
     }
@@ -93,7 +132,7 @@ const CreateTrack: React.FC<any> = ({classes}) => {
       <>
         {/* Create Track BUtton */}
 
-        <Affix style={{ position: "fixed", bottom: 30, right: 100 }}>
+        <Affix style={{ position: "fixed", bottom: 30, right: 100, zIndex:999 }}>
           <Button
             type="primary"
             icon={<FileAddFilled style={{ fontSize: "16px" }} />}
@@ -125,6 +164,7 @@ const CreateTrack: React.FC<any> = ({classes}) => {
           >
             <Input
               prefix="Title"
+              value={title}
               onChange={(e) => setTitle(e.target.value)}
             ></Input>
             <br />
@@ -133,25 +173,18 @@ const CreateTrack: React.FC<any> = ({classes}) => {
               prefix="Description"
               allowClear
               placeholder="Description"
+              value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
             <br />
             <br />
-            <Upload 
-                {...props} 
-                listType="picture"
-                onChange={handleAudioChange}
-               
-            
-            >
+            <Upload {...props} listType="picture" onChange={handleAudioChange} customRequest={uploadAudio}>
               <Button>
                 Click to Upload &nbsp; <UploadOutlined />
               </Button>
             </Upload>
           </Modal>
         )}
-
-  
       </>
     );
 
