0709cf9471619102bc4f8f9752a01918731e1897
Andy Tran <antr900@gmail.com>
2020-06-24 15:01:20 -0700

    Made more changes to gitignire

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from unixtimestampfield.fields import UnixTimeStampField
+
 
 # Create your models here.
 class Track(models.Model):
