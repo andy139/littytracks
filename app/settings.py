5ab7b28eeba8f5f250dc358a55fbd5a7d8eee6db
Andy Tran <antr900@gmail.com>
2020-06-25 15:41:30 -0700

    Changed django heroku

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,7 +172,7 @@ USE_TZ = False
 # Configure app for Heroku deployment
 
 
-# django_heroku.settings(locals())
+
 
 
 STATIC_URL = '/static/'
