2d251ff09ece9d32489343b62bf86ac2cc17268b
Andy Tran <antr900@gmail.com>
2020-06-25 00:26:30 -0700

    Removed unix timestamp from tracks

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'tracks',
+    'whitenoise.runserver_nostatic',  # < As per whitenoise documentation
+    'django.contrib.staticfiles',
+
+    # 3rd party apps
+    'rest_framework',
 
 ]
 
@@ -147,4 +152,18 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Configure app for Heroku deployment
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
+# Place static in the same location as webpack build files
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+STATICFILES_DIRS = []
+
+# If you want to serve user uploaded files add these settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
