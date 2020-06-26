9b1748e9bf14e8628bc15d5a1eb7dcff4ccac214
Andy Tran <antr900@gmail.com>
2020-06-25 17:21:02 -0700

    requirements file

--- app/settings.py
+++ app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -44,11 +45,11 @@ INSTALLED_APPS = [
 
 
 
-    # 'whitenoise.runserver_nostatic',  # < As per whitenoise documentation
-    # 'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',  # < As per whitenoise documentation
+    'django.contrib.staticfiles',
 
-    # # # 3rd party apps
-    # 'rest_framework',
+    # # 3rd party apps
+    'rest_framework',
 
 ]
 
