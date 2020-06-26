9660e19d73b26af3f27b039c771d1362a8da090a
Andy Tran <antr900@gmail.com>
2020-06-25 19:44:29 -0700

    requirements file

--- app/urls.py
+++ app/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import index
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
