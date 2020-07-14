5aac9fadf128e8c534f3643ba43465cc86938e95
Andy Tran <antr900@gmail.com>
2020-07-14 01:38:13 -0700

    Add mini player

--- src/components/Shared/AudioPlayer.tsx
+++ src/components/Shared/AudioPlayer.tsx
@@ -32,9 +32,7 @@ import { ME_QUERY } from '../../App';
 
 import { UserContext } from '../../App';
 import UpdateBackground from './UpdateBackground';
-import './shared.css'
-
-
+import './shared.css';
 
 const { Paragraph } = Typography;
 const CREATE_PLAY_MUTATION = gql`
@@ -48,43 +46,55 @@ const CREATE_PLAY_MUTATION = gql`
 `;
 
 export const GET_COMMENTS_QUERY = gql`
-    query($trackId: Int!){
-            comments(trackId: $trackId) {
-                comment
-                    postedBy {
-                    id
-                    }
-                    createdAt
-                    id
-                    postedBy {
-                    username
-                    id
-                    userprofile {
-                        avatarUrl
-                    }
-            }
-        }
-
-    }
-
-`
+	query($trackId: Int!) {
+		comments(trackId: $trackId) {
+			comment
+			postedBy {
+				id
+			}
+			createdAt
+			id
+			postedBy {
+				username
+				id
+				userprofile {
+					avatarUrl
+				}
+			}
+		}
+	}
+`;
 
 const { SoundCloudLogoSVG, PlayIconSVG, PauseIconSVG, NextIconSVG, PrevIconSVG } = Icons;
 
 const { Header, Footer, Sider, Content } = Layout;
 
 const AudioPlayer: React.FC<any> = withCustomAudio((props) => {
-	const { soundCloudAudio, trackTitle, tracks, match, currentUser, imgUrl, playing, track, currentTime, duration, gifNum, setGifNum } = props;
+	const {
+		soundCloudAudio,
+		trackTitle,
+		tracks,
+		match,
+		currentUser,
+		imgUrl,
+		playing,
+		track,
+		currentTime,
+		duration,
+		gifNum,
+		setGifNum
+	} = props;
 	const [ createPlay, { data, loading } ] = useMutation(CREATE_PLAY_MUTATION, {
 		refetchQueries: [ { query: ME_QUERY } ]
 	});
 
 	const [ scrolling, setScroll ] = useState(true);
 
-	const [modal, setModal] = useState(false);
-	
+	const [ minimizePlayer, setMinimize ] = useState(true);
 
-	debugger
+	const [ modal, setModal ] = useState(false);
+
+	debugger;
 
 	const setTrackModal = () => {
 		setScroll(false);
@@ -124,18 +134,45 @@ const AudioPlayer: React.FC<any> = withCustomAudio((props) => {
 		<Modal
 			title={
 				// null
-				<div className='modal-title-space'>
-					<Link to="/" style={{}}
-						onClick={() => disableTrackModal()}
-					>
+				<div className="modal-title-space">
+					<Link to="/" style={{}} onClick={() => disableTrackModal()}>
 						{/* <Title level={4} className='logo-font' style={{ color: "#d2e0fa" }}>LittyTracks</Title> */}
 						<img src="https://django-app-images.s3-us-west-1.amazonaws.com/logo.png" style={{ width: 150 }} />
 					</Link>
+					{!minimizePlayer ? (
+						<span className="top-music-player">
+							<div className="music-settings">
+								<span className="playButton3" style={{}} onClick={() => play()}>
+									{playing ? (
+										<span>
+											{' '}
+											<PauseIconSVG />
+										</span>
+									) : (
+										<span>
+											<PlayIconSVG />
+										</span>
+									)}
+								</span>
+								&nbsp;
+								<VolumeControl
+									className="music-settings"
+									buttonClassName="volume-button2"
+									rangeClassName="range-volume"
+									{...props}
+								/>
+								<Progress {...props} className="bar-radius2" innerClassName="bar-color2" />
+								{/* <Timer {...props} className="timer-top" /> */}
+							</div>
+						</span>
+					) : null}
 
-					<i className="fas fa-times-circle"
-						style={{ fontSize: 45, cursor:'pointer' }}
-						onClick={() => disableTrackModal()}/>
-						
+					<i
+						className="fas fa-times-circle"
+						style={{ fontSize: 45, cursor: 'pointer' }}
+						onClick={() => disableTrackModal()}
+					/>
+
 					{/* <Button
 						style={{
 							margin: '15px'
@@ -145,24 +182,18 @@ const AudioPlayer: React.FC<any> = withCustomAudio((props) => {
 						}
 					/> */}
 				</div>
-		
-
 			}
 			visible={modal}
 			onCancel={() => disableTrackModal()}
 			width="100%"
-		
 			style={{
 				top: 0,
 				margin: 0,
 				position: 'fixed'
-			
 			}}
 			closable={false}
 			footer={null}
-			bodyStyle={{ height: '100vh', overflowY: 'auto', padding:'0px' }}
-			
-			
+			bodyStyle={{ height: '100vh', overflowY: 'auto', padding: '0px' }}
 		>
 			{/* <Row className="player-background">
 				<Button
@@ -172,101 +203,109 @@ const AudioPlayer: React.FC<any> = withCustomAudio((props) => {
 				/>
 			</Row> */}
 
-
-			<Row justify={'center'} align={'middle'}className='full-row'> 
-			
+			<Row justify={'center'} align={'middle'} className="full-row">
 				<Col span={16} className="col-padding col-blurry center-col row-modal">
 					<div
-						className='player-background'
+						className="player-background"
 						style={{
-							backgroundImage:`url(${currentUser.userprofile.backgroundUrl})`
+							display: 'flex',
+							justifyContent: 'space-between',
+							backgroundImage: `url(${currentUser.userprofile.backgroundUrl})`
 						}}
-					
-					
 					>
-						<UpdateBackground/>
-					
-
+		
 					</div>
 
-					<div className="player-modal over">
+					{minimizePlayer ? (
+						<div className="player-modal over">
+							<Row align="middle" justify="center" gutter={[ 24, 16 ]}>
+								<Col flex="0 1 400px" style={{ justifyContent: 'center', display: 'flex' }}>
+									<img
+										width={300}
+										className="imgTrack"
+										alt="logo"
+										src={
+											imgUrl ? imgUrl : 'http://res.cloudinary.com/andytran/raw/upload/v1592239178/ksa9qczmaoicuqcgdo10'
+										}
+									/>
+								</Col>
 
-						<Row align="middle" justify="center" gutter={[24, 16]}>
-							<Col flex="0 1 400px" style={{ justifyContent: 'center', display: 'flex' }}>
-								<img
-
-									width={300}
-									className="imgTrack"
-									alt="logo"
-									src={imgUrl ? imgUrl : 'http://res.cloudinary.com/andytran/raw/upload/v1592239178/ksa9qczmaoicuqcgdo10'}
-								/>
-								
-							</Col>
-						
-
-							<Col flex="1 1 200px">
-								<h2>Track</h2>
-
-								<h1 style={{ fontSize: 40 }}>{trackTitle}</h1>
-								<h2>By {track.artistName}</h2>
-								&nbsp;
-								<Timer {...props} className="timer" />
-
-								<span className="playButton2" onClick={() => play()}>
-									{playing ? (
-										<span>
-											{' '}
-											<PauseIconSVG />&nbsp;&nbsp; PAUSE
-										</span>
-									) : (
+								<Col flex="1 1 200px">
+									<h2>Track</h2>
+									<h1 style={{ fontSize: 40 }}>{trackTitle}</h1>
+									<h2>By {track.artistName}</h2>
+									&nbsp;
+									<Timer {...props} className="timer" />
+									<span className="playButton2" onClick={() => play()}>
+										{playing ? (
+											<span>
+												{' '}
+												<PauseIconSVG />&nbsp;&nbsp; PAUSE
+											</span>
+										) : (
 											<span>
 												<PlayIconSVG />&nbsp;&nbsp;PLAY
 											</span>
 										)}
-								</span>
-
-								<div className="music-settings">
-									<VolumeControl
-										className="music-settings"
-										buttonClassName="volume-button"
-										rangeClassName="range-volume"
-										{...props}
-									/>
-									<Progress {...props} className="bar-radius" innerClassName="bar-color" />
-								</div>
-
-								<h1>{track.plays.length} Plays</h1>
-
-							</Col>
-
-
-						</Row>
-
-
-					</div>
-					
+									</span>
+									<div className="music-settings">
+										<VolumeControl
+											className="music-settings"
+											buttonClassName="volume-button"
+											rangeClassName="range-volume"
+											{...props}
+										/>
+										<Progress {...props} className="bar-radius" innerClassName="bar-color" />
+									</div>
+									<h1>{track.plays.length} Plays</h1>
+								</Col>
+							</Row>
+						</div>
+					) : null}
 
 					{/* Music Player */}
 				</Col>
-				<Col span={8} className="col-modal-right" >
-				
-					<Row>
-						<h2>
+				<Col span={8} className="col-modal-right">
+					<Row style={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						marginBottom: '10px'
+					}}>
+						<h2 style={{
+							marginBottom: 0,
+								display: 'flex',
+							alignItems: 'center'
+						}}>
 							<Link to={`/profile/${track.postedBy.id}`}>
 								<Avatar size="large" icon={<img src={track.postedBy.userprofile.avatarUrl} />} />
 								&nbsp; {track.postedBy.username}
 							</Link>
 						</h2>
+
+						<span className="player-options">
+							<Button
+								onClick={() => setMinimize(!minimizePlayer)}
+								style={{
+									margin: '10px',
+									// opacity: '0.6'
+								}}
+								type="primary"
+								shape="round"
+							>
+								{minimizePlayer ? 'Minimize Player' : 'Maximize Player'}
+							</Button>
+
+							<UpdateBackground />
+
+						</span>
+					
 					</Row>
 					<Paragraph>{track.description}</Paragraph>
 					<LikeTrack trackId={track.id} likeCount={track.likes.length} />
 					&nbsp;
 					<CommentTrack track={track} commentCount={track.comments.length} setModal={setTrackModal} />
 					<Divider />
-				
-
 					<CommentList trackId={track.id} comments={track.comments} />
-
 					{/* <div className="player-modal over">
 
 						<Row align="middle" justify="center" gutter={[24, 16]}>
